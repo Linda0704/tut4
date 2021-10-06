6c4097daa619e92ec86e403caae6fef3c86d8142
Linda Chen <949996504@qq.com>
2021-10-07 01:13:08 +0800

    again

---
+++ src/main/java/tut4/Calc.java
@@ -0,0 +1,20 @@
+package tut4;
+
+
+public class Calc
+{
+    public static void main(String[] args) {
+        System.out.println(add(2,3));
+        System.out.println(subtract(7,4));
+    }
+
+    public static int add(int a, int b){
+        int sum = a + b;
+        return sum;
+    }
+
+    public static int subtract(int a, int b){
+        int sub = a + b;
+        return sub;
+    }
+}
---
+++ src/test/java/tut4/CalcTest.java
@@ -0,0 +1,54 @@
+package tut4;
+import tut4.Calc;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class CalcTest{
+
+    Calc calc = new Calc();
+
+    @Test
+    void testAdd() {
+        int a = calc.add(2, 3);
+        assertTrue(a==5);
+    }
+
+//    @Test
+//    void testSubtract() {
+//        int a = calc.add(5, 3);
+//        assertTrue(a==2);
+//    }
+
+}
+//    extends TestCase
+//{
+//    /**
+//     * Create the test case
+//     *
+//     * @param testName name of the test case
+//     */
+//    public CalcTest(String testName )
+//    {
+//        super( testName );
+//    }
+//
+//    /**
+//     * @return the suite of tests being tested
+//     */
+//    public static Test suite()
+//    {
+//        return new TestSuite( CalcTest.class );
+//    }
+//
+//    /**
+//     * Rigourous Test :-)
+//     */
+//    public void testApp()
+//    {
+//        assertTrue( true );
+//    }
+//}
